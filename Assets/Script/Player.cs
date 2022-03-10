28feae9fefe7a636d61ea17e1a2be013061d7b80
Haoyu Chen <haoyuchen@fb.com>
2022-03-09 19:18:22 -0800

    Add script for player movement and collision

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(Vector3.forward * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Translate(Vector3.back * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Rotate(Vector3.up, -10);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Rotate(Vector3.up, 10);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.name == "Coin")
+        {
+            Destroy(collision.collider.gameObject);
+        }
+    }
+}
